61162358554f11f73cc06507b20c7c07d19b3415
nageswar8 <nageswarreddy447@gmail.com>
2025-09-22 07:53:35 +0530

    committed

---
+++ 03-calculate.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo date
+
+time=$(date +%s)
+
---
+++ 06-install-mysql.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+$USER=$(id -u)
+
+if [ $USER -ne 0] then
+    echo "Error :: Please run with root access"
+fi
+
+
+dnf install mysql -y
+
+
+if [ $? -ne 0 ] then
+    echo "Error installing mysql"
+else
+    echo "Installed success
+fi
