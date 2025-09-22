b7653012aa5d6a1fa47b1c3731c889ca24b51f63
nageswar8 <nageswarreddy447@gmail.com>
2025-09-22 08:19:41 +0530

    shell script

--- 06-install-mysql.sh
+++ 06-install-mysql.sh
@@ -2,16 +2,24 @@
 
 USER=$(id -u)
 
-if [ $USER -ne 0 ]; then
-    echo "Error :: Please run with root access"
-fi
+VALIDATE() {
+
+    if [ $1 -ne 0 ]; then
+        echo "Error :: Please run with root access"
+        exit 1
+    fi
 
 
-dnf install mysql -y
+    dnf install $2 -y
 
 
-if [ $? -ne 0 ]; then
-    echo "Error installing mysql"
-else
-    echo "Installed success"
-fi
+    if [ $? -ne 0 ]; then
+        echo "Error installing $2"
+        exit 1
+    else
+        echo "Installed success"
+    fi
+}
+
+VALIDATE $USER "mysql"
+
