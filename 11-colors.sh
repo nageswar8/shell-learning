4ff073b413c93fe06754b64f2ca482ab343fabb9
nageswar8 <nageswarreddy447@gmail.com>
2025-09-23 22:19:33 +0530

    shell script

--- 11-colors.sh
+++ 11-colors.sh
@@ -13,6 +13,11 @@ if [ $USERID -ne 0 ]; then
     exit 1 
 fi
 
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
+fi
+
 VALIDATE() {
 
     if [ $1 -ne 0 ]; then
