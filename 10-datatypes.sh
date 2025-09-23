df1610af0eb7a34cb77bc189ed773e468e31bc4a
nageswar8 <nageswarreddy447@gmail.com>
2025-09-23 21:56:21 +0530

    shell script

---
+++ 10-datatypes.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+NAME="NAGESH"
+NUMBER=10
+NUM2=20
+
+SUM=$(NUM2+NUMBER)
+
+echo $SUM
+
+SUM2=$(NUM2+NUMBER+NAME)
+
+echo " another sum $SUM2"
+
+#ARRAY
+
+LEADERS=("MODU", "GODU", "JODU")
+
+echo "print all ${LEADERS[@]}"
+echo "First Leader: ${LEADERS[0]}"
+echo "First Leader: ${LEADERS[10]}" 
